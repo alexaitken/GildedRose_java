c36b5d1e49aa03204995a95cb957d7d8163c8df6
Alex Aitken <alex@alexaitken.com>
2011-02-24 09:09:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# The Gilded Rose Code Kata
+
+This is a Java version of the Gilded Rose Kata, original found
+[here](http://www.iamnotmyself.com/2011/02/13/RefactorThisTheGildedRoseKata.aspx).
+
+
+## A few small changes
+
+* After seeing jim weirich [ruby port](http://github.com/jimweirich/gilded_rose_kata) I also included tests in the project for the original functionality.
+
+* I have moved the hard coded items to the default constructor and gave an alternative constructor to allows the tests to inject the list of items.
+
+
+## Setup
+
+This has been setup as an eclipse project.  I have not provided an ant build file or a maven pom.
+* To setup create a new java project in eclipse.  (check out the code from git hub directly or import it into your java project)
+* add the junit 4 library to your build path.
+* include both the test and src directory as "Source Folders"
+* right click the project and run as JUnit test
+
+
+## Branches
+
+* The 'master' branch contains the starting point for the kata.  It is
+  also tagged as 'start-here'.
+
+* The 'solution' branch is my first solution for this kata. Feedback welcome!
+
+It was fun. You should give it a try!
+
+Alex    
+ 
+
+# Original Description of the Gilded Rose
+
+Hi and welcome to team Gilded Rose. As you know, we are a small inn
+with a prime location in a prominent city ran by a friendly innkeeper
+named Allison. We also buy and sell only the finest
+goods. Unfortunately, our goods are constantly degrading in quality as
+they approach their sell by date. We have a system in place that
+updates our inventory for us. It was developed by a no-nonsense type
+named Leeroy, who has moved on to new adventures. Your task is to add
+the new feature to our system so that we can begin selling a new
+category of items. First an introduction to our system:
+
+- All items have a SellIn value which denotes the number of days we
+  have to sell the item
+- All items have a Quality value which denotes how valuable the item
+  is
+- At the end of each day our system lowers both values for every item
+
+Pretty simple, right? Well this is where it gets interesting:
+
+  - Once the sell by date has passed, Quality degrades twice as fast
+  - The Quality of an item is never negative
+  - "Aged Brie" actually increases in Quality the older it gets
+  - The Quality of an item is never more than 50
+  - "Sulfuras", being a legendary item, never has to be sold or
+    decreases in Quality
+  - "Backstage passes", like aged brie, increases in Quality as it's
+    SellIn value approaches; Quality increases by 2 when there are 10
+    days or less and by 3 when there are 5 days or less but Quality
+    drops to 0 after the concert
+
+We have recently signed a supplier of conjured items. This requires an update to our system:
+
+- "Conjured" items degrade in Quality twice as fast as normal items
+
+Feel free to make any changes to the UpdateQuality method and add any
+new code as long as everything still works correctly. However, do not
+alter the Item class or Items property as those belong to the goblin
+in the corner who will insta-rage and one-shot you as he doesn't
+believe in shared code ownership (you can make the UpdateQuality
+method and Items property static if you like, we'll cover for
+you). Your work needs to be completed by Friday, February 18, 2011
+08:00:00 AM PST.
+
+Just for clarification, an item can never have its Quality increase
+above 50, however "Sulfuras" is a legendary item and as such its
+Quality is 80 and it never alters.
---
+++ test/test/com/alexaitken/gildedrose/InventoryTest.java
@@ -0,0 +1,193 @@
+package test.com.alexaitken.gildedrose;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.alexaitken.gildedrose.Inventory;
+import com.alexaitken.gildedrose.Item;
+
+
+public class InventoryTest {
+//	new Item("+5 Dexterity Vest", 10, 20), 
+//	new Item("Aged Brie", 2, 0),
+//	new Item("Elixir of the Mongoose", 5, 7),
+//	new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+//	new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+//	new Item("Conjured Mana Cake", 3, 6)
+	
+	@Test
+	public void should_never_changes_quailty_of_Sulfuras() throws Exception {
+		Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(new Item("Sulfuras, Hand of Ragnaros", 0, 80)).toArray());
+		
+		sut.updateQuality();
+		
+		assertEquals(80, sulfuras.getQuality());
+		
+		
+	}
+	@Test
+	public void should_never_changes_sellIn_of_Sulfuras() throws Exception {
+		Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(sulfuras).toArray());
+		
+		sut.updateQuality();
+		
+		assertEquals(0, sulfuras.getSellIn());
+		
+		
+	}
+	
+	
+	
+	@Test
+	public void should_lower_the_sellIn_by_one_for_normal_items() throws Exception {
+		Item normalItem = new Item("+5 Dexterity Vest", 10, 20);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(9, normalItem.getSellIn());
+	}
+	
+	@Test
+	public void should_lower_the_quality_by_one_for_normal_items() throws Exception {
+		Item normalItem = new Item("+5 Dexterity Vest", 10, 20);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(19, normalItem.getQuality());
+	}
+	
+	@Test
+	public void should_not_lower_the_quality_below_zero() throws Exception {
+		Item normalItem = new Item("+5 Dexterity Vest", 10, 0);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(0, normalItem.getQuality());
+	}
+	
+	@Test
+	public void should_lower_the_quality_twice_as_fast_once_the_sell_in_date_has_passed() throws Exception {
+		Item normalItem = new Item("+5 Dexterity Vest", -1, 25);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(normalItem).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(23, normalItem.getQuality());
+	}
+	
+	
+	@Test
+	public void should_increase_the_quality_of_aged_brie_as_it_gets_older() throws Exception {
+		Item agedBrie = new Item("Aged Brie", 10, 25);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(agedBrie).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(26, agedBrie.getQuality());
+	}
+	
+	
+	@Test
+	public void should_not_increase_the_quality_of_aged_brie_over_50() throws Exception {
+		Item agedBrie = new Item("Aged Brie", 10, 50);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(agedBrie).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(50, agedBrie.getQuality());
+	}
+	
+	@Test
+	public void should_lower_backstage_passes_to_zero_quality_once_concert_has_happened() throws Exception {
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(0, backStagePass.getQuality());
+	}
+	
+	@Test
+	public void should_increase_backstage_passes_quality_by_1_when_the_concert_is_more_than_10_days_away() throws Exception {
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(21, backStagePass.getQuality());
+	}
+	
+	@Test
+	public void should_increase_backstage_passes_quality_by_2_when_the_concert_is_10_days_or_less_away() throws Exception {
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 27);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(29, backStagePass.getQuality());
+	}
+	
+	@Test
+	public void should_increase_backstage_passes_quality_by_3_when_the_concert_is_5_days_or_less_away() throws Exception {
+		Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 44);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePass).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(47, backStagePass.getQuality());
+	}
+	
+	@Test
+	public void should_not_increase_backstage_passes_above_a_quality_of_50() throws Exception {
+		Item backStagePassMoreThan10DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50);
+		
+		Item backStagePass10DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49);
+		Item backStagePass5DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48);
+		
+		Inventory sut = new Inventory((Item[]) Arrays.asList(backStagePassMoreThan10DaysAway, backStagePass10DaysAway, backStagePass5DaysAway).toArray());
+		
+		sut.updateQuality();
+		
+		
+		assertEquals(50, backStagePassMoreThan10DaysAway.getQuality());
+		assertEquals(50, backStagePass10DaysAway.getQuality());
+		assertEquals(50, backStagePass5DaysAway.getQuality());
+	}
+	
+	
+	
+	
+	
+	
+	
+}
